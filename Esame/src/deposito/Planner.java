9701db867b10fb5a482c34adb222aef1d06a1648
Cosomazio <73337360+Cosomazio@users.noreply.github.com>
2020-11-26 10:27:10 +0100

    Inizializzazione

--- Esame/src/deposito/Planner.java
+++ Esame/src/deposito/Planner.java
@@ -4,6 +4,7 @@
  * and open the template in the editor.
  */
 package deposito;
+import java.sql.*;
 import java.time.*;
 import java.util.*;
 /**
@@ -37,9 +38,29 @@ public class Planner extends AbstractUtente {
             Procedure procedura){
         
     }
-    public void modifyActivity(InterfaceActivity act){
+    public void modifyActivity(InterfaceActivity act,int id, Sito sito,String tipologia, String descrizione, int tempo, 
+            List<String> materiali, int week, Boolean interrompibile, Procedure procedura){
         
-    }
+        int idAct=act.getId();
+        String url="kandula.db.elephantsql.com";
+        String user="figslypy";
+        String pass="lwHyJdBS_3DZCU4mlrffKxLP7hwmyZio";
+        
+        try{
+            //creo la connessione al DB
+            Connection c= DriverManager.getConnection(url,user,pass);
+            
+            String query= "UPDATE Attivita SET id="+id+",sito="+sito+",tipologia="+tipologia+",descrizione="+descrizione+",tempo="+tempo+",materiali="+materiali+
+                    ",week="+week+",interrompibile="+interrompibile+",procedura="+procedura+" WHERE id="+idAct;
+            PreparedStatement st = c.prepareStatement(query);
+            ResultSet rs=st.executeQuery();
+            st.close();
+            rs.close();
+            c.close();
+            
+        }catch(SQLException ex){
+            System.out.println("ERRORE DATABASE");
+        }
     public void deleteActivity(InterfaceActivity act){
         
     }
