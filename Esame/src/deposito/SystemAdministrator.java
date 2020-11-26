d4e83e2395f179b3d97f707e455edff10107d88b
Cosomazio <73337360+Cosomazio@users.noreply.github.com>
2020-11-26 09:42:04 +0100

    Inizializzazione

---
+++ Esame/src/deposito/SystemAdministrator.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package deposito;
+
+/**
+ *
+ * @author cosimocorbisiero
+ */
+public class SystemAdministrator extends AbstractUtente {
+    
+    public SystemAdministrator(String username,String password,String nome,String email,int id){
+        super(username,password,nome,email,id);
+    }
+    
+    
+    
+    
+    
+}
