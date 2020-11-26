d17091c6bf3b91e79ea53ca145406fac629f9860
franc <franc@LAPTOP-2RV6G94A>
2020-11-26 11:07:09 +0100

    classi del package factories

---
+++ Esame/src/factories/UtenteFactory.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package factories;
+
+import deposito.*;
+
+/**
+ *
+ * @author franc
+ */
+public abstract class UtenteFactory {
+    
+    public static AbstractUtente make(Category ut, String nome, String username, String password,String email){
+         UtenteFactory factory = null;
+         
+        if (ut==Category.PLANNER){
+            factory= new PlannerFactory()
+        }
+        if (ut == Category.MAINTAINER){
+            factory = new MaintainerFactory()
+        }
+        if (ut == Category.SYSTEMADMINISTRATOR)
+            factory = new SaFactory()
+                    
+        return factory.build(ut, nome, username, password, email);
+    }
+    
+    protected abstract AbstractUtente build();
+}
