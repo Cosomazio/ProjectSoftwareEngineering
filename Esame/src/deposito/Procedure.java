781ae369b3f4ff42c9b5a451ce5b53f068d439c7
wwertyexp <ilcditommy@gmail.com>
2020-11-27 18:09:33 +0100

    procedure fix

--- Esame/src/deposito/Procedure.java
+++ Esame/src/deposito/Procedure.java
@@ -13,16 +13,26 @@ import java.util.Set;
  * @author cosimocorbisiero
  */
 public class Procedure {
+    private String nomefile;
     private File smp;
     private Set<String> competenze;
 
-    public Procedure(File smp, Set<String> competenze) {
+    public Procedure(File smp, Set<String> competenze,String nomefile) {
         this.smp = smp;
         this.competenze = competenze;
+        this.nomefile=nomefile;
     }
 
     public Procedure(){
         
+    }
+    
+    public String getNomefile() {
+        return nomefile;
+    }
+
+    public void setNomefile(String nomefile) {
+        this.nomefile = nomefile;
     }
     
     public File getSmp() {
