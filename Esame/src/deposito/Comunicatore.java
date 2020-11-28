5b92d4ff42d0eda41722f10247e0ce28e6683c00
franc <franc@LAPTOP-2RV6G94A>
2020-11-28 11:17:25 +0100

    modifiche Comunicatore + updateQuery

--- Esame/src/deposito/Comunicatore.java
+++ Esame/src/deposito/Comunicatore.java
@@ -4,6 +4,7 @@
  * and open the template in the editor.
  */
 package deposito;
+
 import java.sql.*;
 
 import java.util.HashMap;
@@ -15,100 +16,132 @@ import java.util.ArrayList;
  * @author franc
  */
 public class Comunicatore {
+
     private Connection c;
 
     public Comunicatore() throws SQLException {
         this.c = DriverManager.getConnection("jdbc:postgresql://kandula.db.elephantsql.com:5432/figslypy", "figslypy", "lwHyJdBS_3DZCU4mlrffKxLP7hwmyZio");
     }
-    
-    public int insertQuery(String table, HashMap<String, Object> params){
-        int res=-1;
+
+    public int insertQuery(String table, HashMap<String, Object> params) {
         int i, j;
-        String query="insert INTO"+ " "+table+ " (";
-        Set<String> k=params.keySet();
-        ArrayList<String> l= new ArrayList<>();
-        for(String chiave: k){
+        String query = "insert INTO" + " " + table + " (";
+
+        ArrayList<String> l = preparazione(params.keySet());
+
+        for (i = 0; i < l.size() - 1; i++) {
+            query = query + l.get(i) + ",";
+        }
+        query = query + l.get(i) + ")";
+        String values = " values (";
+        for (j = 0; j < l.size() - 1; j++) {
+            Object par = params.get(l.get(j));
+            if (par instanceof Integer || par instanceof Boolean) {
+                values = values + "" + par + "" + ",";
+            } else {
+                values = values + "'" + par + "'" + ",";
+            }
+        }
+        values = values + "'" + params.get(l.get(j)) + "'" + ")";
+        System.out.println(query + values);
+
+        return eseguiUpdate(query + values);
+    }
+
+    public int deleteQuery(String table, HashMap<String, Object> params) {
+        int i = 0;
+        Object p;
+
+        String query = "DELETE FROM " + table;
+        String values = " WHERE ";
+
+        ArrayList<String> l = preparazione(params.keySet());
+
+        if (l.size() > 1) {
+            for (i = 0; i < l.size() - 1; i++) {
+                p = params.get(l.get(i));
+                if (p instanceof String) {
+                    p = "'" + p + "'";
+                }
+                values = values + l.get(i) + " = " + p + " and ";
+            }
+        }
+            p = params.get(l.get(i));
+            if (p instanceof String)
+                p = "'" + p + "'";
+            values = values + l.get(i) + " = " + p;
+        
+
+        System.out.println(query + values);
+        return eseguiUpdate(query + values);
+    }
+
+    public int updateQuery(String table, HashMap<String, Object> params, HashMap<String, Object> chiavi) {
+        int i = 0;
+        Object p;
+
+        String query = "UPDATE " + table;
+        String values = " SET ";
+        ArrayList<String> l = preparazione(params.keySet());
+        ArrayList<String> keys = preparazione(chiavi.keySet());
+
+        if (l.size() > 1) {
+            for (i = 0; i < l.size() - 1; i++) {
+                p = params.get(l.get(i));
+                if (p instanceof String) {
+                    p = "'" + p + "'";
+                }
+                values = values + l.get(i) + " = " + p + ", ";
+            }
+        }
+        p = params.get(l.get(i));
+        if (p instanceof String) {
+            p = "'" + p + "'";
+        }
+        values = values + l.get(i) + " = " + p;
+
+        values = values + " WHERE ";
+
+        if (keys.size() > 1) {
+            for (i = 0; i < keys.size() - 1; i++) {
+                p = chiavi.get(keys.get(i));
+                if (p instanceof String) {
+                    p = "'" + p + "'";
+                }
+                values = values + keys.get(i) + " = " + p + " and ";
+            }
+        }
+        p = chiavi.get(keys.get(i));
+        if (p instanceof String) {
+            p = "'" + p + "'";
+        }
+        values = values + keys.get(i) + " = " + p;
+
+        System.out.println(query + values);
+
+        return eseguiUpdate(query + values);
+    }
+
+    private ArrayList<String> preparazione(Set<String> set) {
+        ArrayList<String> l = new ArrayList<>();
+        for (String chiave : set) {
             l.add(chiave);
         }
-        
-        for(i=0; i<k.size()-1; i++){
-            query=query + l.get(i)+",";
-        }
-        query=query+l.get(i)+")";
-        String values=" values (";
-        for(j=0; j<k.size()-1; j++){
-            Object par=params.get(l.get(j));
-            if (par instanceof Integer || par instanceof Boolean){
-                values=values+""+par+""+",";
-            }
-            else{
-            values=values+"'"+par+"'"+",";
-            }
-        }
-        values=values+"'"+params.get(l.get(j))+"'"+")";
-        System.out.println(query+values);
-        try {    
+        return l;
+    }
+
+    private int eseguiUpdate(String query) {
+        int res = -1;
+        try {
             Statement pt = c.createStatement();
-            res=pt.executeUpdate(query+values);
-        
+            res = pt.executeUpdate(query);
         } catch (SQLException ex) {
             System.out.println("ERRORE NELLA QUERY");
         }
-        
         return res;
     }
-    
-    public int deleteQuery(String table, HashMap<String, Object> params){
-        int res=-1;
-        int i=0,j;
-        Object p;
-        
-        String query="DELETE FROM "+table;
-        String values= " WHERE ";
-        
-        Set<String> k=params.keySet();
-        ArrayList<String> l= new ArrayList<>();
-        for(String chiave: k){
-            l.add(chiave);
-        }
-        
-        if(k.size()>1){
-            for(i=0; i<k.size()-1; i++){
-                p = params.get(l.get(i));
-                if(p instanceof String)
-                    p="'"+p+"'";
-                values = values+l.get(i)+ " = "+ p + " and ";
-        }
-            p=params.get(l.get(i));
-            if(p instanceof String)
-                p="'"+p+"'";
-            values = values+l.get(i)+ " = "+ p;
-        }
-        else{
-            p=params.get(l.get(i));
-            if(p instanceof String)
-                p="'"+p+"'";
-            values= values+ l.get(i)+ " = "+ p;
-        }
-        
-        System.out.println(query+values);
-        
-        try{
-            Statement pt = c.createStatement();
-            res=pt.executeUpdate(query+values);
-        }catch(SQLException ex){
-            System.out.println("LA QUERY è ANDATA IN ERRORE");
-        }
-        
-        return res;
-    }
-    
-    public int updateQuery(String table, HashMap<String, Object> params){
-        return 2;
-    }
-    
-    
-    public void chiudi() throws SQLException{
+
+    public void chiudi() throws SQLException {
         this.c.close();
     }
 
